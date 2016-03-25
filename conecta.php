ff7e50cb5aed004a3c4ab14bc07e92e6245a6982
Ana Có <ana.lock14@gmail.com>
2016-03-25 12:41:45 -0300

    Alterações layout
    [Falta alterar logo]

--- conecta.php
+++ conecta.php
@@ -1,7 +1,24 @@
 <?php
-	$hostname = "localhost";
-	$username = "hermes";
-	$password = "R@mon19";
+
+
+	if ($_SERVER['HTTP_HOST'] == 'localhost') {
+
+		$path = 'http://localhost/Projects/hermes';
+		$hostname = "localhost";
+		$username = "root";
+		$password = "";
+		$database = "hermes";
+
+	}
+	else{
+
+		$path = 'http://www.hermesacademico.com.br';
+		$hostname = "localhost";
+		$username = "penna495_root";
+		$password = "R@mon1901";
+		$database = "penna495_hermes";
+
+	}
 
 	// Create connection
 	// $conn = new mysqli($servername, $username, $password);
@@ -9,13 +26,6 @@
 	$conn = mysql_connect($hostname, $username, $password) or die("Unable to connect to MySQL");
 	// echo "Connected to MySQL<br>";
 
-	$selected = mysql_select_db("penna495_hermes",$conn) or die("Could not select hermes");
+	$selected = mysql_select_db($database,$conn) or die("Could not select hermes".mysql_error());
 	// echo "hermes Selected";
-
-	if ($_SERVER['HTTP_HOST'] == 'localhost') {
-		$path = 'http://localhost/Projects/hermes';
-	}
-	else{
-		$path = 'http://www.hermesacademico.com.br';
-	}
 ?>
