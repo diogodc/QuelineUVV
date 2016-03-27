4cc0520c94a07abe39f913f4c4723a4c5138480e
Ana Có <ana.lock14@gmail.com>
2016-03-26 23:18:30 -0300

    Home, alert plugin,.
    - Início desenvolvimento da Home;
    - Plugin Sweet Alert adicionado;

--- main.php
+++ main.php
@@ -1,2 +1,71 @@
-<h1>Hermes - home </h1>
-<a href="logout.php" id="logout">Logout</a>
+<nav class="navbar navbar-default"> 
+	<div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">
+				<img src="lib/img/logo-hermes.png" alt="Hermes - Logo">
+			</a>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+    	<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav">
+	        	<li class="active">
+	        		<a href="logout.php" id="logout">Sair <span class="sr-only">(current)</span></a></li>
+	        </ul>
+		</div>
+	</div>
+</nav>
+
+<div class="wrapper-content main">
+	<div class="container-fluid">
+		<div class="row">
+			
+			<div class="col-md-3 bg-flat-color-light-gray dark-container">
+				<div class="navigation">
+					<div class="row bg-flat-color-medium-gray">
+						<div class="info-profile center-block text-center">
+							<img src="lib/img/profile.gif" alt="imagem aluno">
+							<p class="name">Ana Karolina Có</p>
+							<p class="detais">Sistemas de Informação - Noturno</p>
+						</div>
+					</div>
+					
+					<div class="row">
+						<ul class="nav nav-pills nav-stacked">
+							<li>
+								<a href="#">Home</a>
+							</li>
+							<li>
+								<a href="#">Perfil</a>
+							</li>
+							<li>
+								<a href="#">Monitores</a>
+							</li>
+							<li>
+								<a href="#">Solicitações</a>
+							</li>
+						</ul>
+					</div>
+					<div class="row bg-flat-color-medium-gray text-center">
+						<div class="logout">
+							<a href="logout.php" id="logout">Sair</a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-9">
+				<section class="wrapper timeline">
+					<h1>Lorem ipsum dolor sit amet</h1>
+				</section>
+			</div>
+		</div>
+	</div>
+</div>
